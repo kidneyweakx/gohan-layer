17f404f454f7c17f93361cc075f454d89898296f
kidneyweakx <kidneyweakx@gmail.com>
2024-05-03 22:41:05 +1000

    feat(circuits): copy sp1 program boilerplate

---
+++ apps/circuits/README.md
@@ -0,0 +1,4 @@
+# SP1 Program
+clone from https://github.com/succinctlabs/sp1/tree/main/examples/ssz-withdrawals
+
+### 
---
+++ apps/circuits/script/src/main.rs
@@ -0,0 +1,24 @@
+use sp1_sdk::{utils, ProverClient, SP1Stdin};
+
+const ELF: &[u8] = include_bytes!("../../program/elf/riscv32im-succinct-zkvm-elf");
+
+fn main() {
+    // Generate proof.
+    // utils::setup_tracer();
+    utils::setup_logger();
+
+    let stdin = SP1Stdin::new();
+    let client = ProverClient::new();
+    let (pk, vk) = client.setup(ELF);
+    let proof = client.prove(&pk, stdin).expect("proving failed");
+
+    // Verify proof.
+    client.verify(&proof, &vk).expect("verification failed");
+
+    // Save proof.
+    proof
+        .save("proof-with-pis.json")
+        .expect("saving proof failed");
+    println!("proof saved to proof-with-pis.json");
+    println!("successfully generated and verified proof for the program!")
+}
