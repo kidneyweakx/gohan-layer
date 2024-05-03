b9fee671f26d236a4b4b0d2fa668fd43c283e358
kidneyweakx <kidneyweakx@gmail.com>
2024-05-03 22:01:35 +1000

    feat(contract): erc20 template in repo

---
+++ packages/contract/scripts/tasks/deployProcess.ts
@@ -0,0 +1,21 @@
+import { task } from "hardhat/config"
+import { readFileSync, writeFileSync } from "../helpers/pathHelper"
+task("deploy:contract", "Deploy contract")
+  .addParam("contract")
+  .setAction(async ({ contract }, hre) => {
+    await hre.run("compile")
+    const [signer]: any = await hre.ethers.getSigners()
+    const contractFactory = await hre.ethers.getContractFactory(contract)
+    // if you mint in constructor, you need to add value in deploy function
+    const deployContract: any = await contractFactory.connect(signer).deploy()
+    console.log(`TestToken.sol deployed to ${deployContract.address}`)
+
+    const address = {
+      main: deployContract.address,
+    }
+    const addressData = JSON.stringify(address)
+    writeFileSync(`scripts/address/${hre.network.name}/`, "mainContract.json", addressData)
+
+    await deployContract.deployed()
+  },
+  )
